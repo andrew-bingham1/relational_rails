7257feace34e382cb4f324664be18500b3477794
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-22 16:04:08 -0600

    feat: complete parent show page, user story 2 complete

--- spec/features/stars/show_spec.rb
+++ spec/features/stars/show_spec.rb
@@ -13,13 +13,29 @@ RSpec.describe 'stars show page' do
     let!(:star_3) {Star.create! name: "Betelgeuse", star_type:"red supergiant", age: 0.0085, size: "2.188 to 10^31 kg", can_nova:true }
 
     it 'can display star attributes' do 
-      visit "/stars/#{star.id}"
+      visit "/stars/#{star_1.id}"
 
       expect(page).to have_content(star_1.name)
       expect(page).to have_content(star_1.star_type)
       expect(page).to have_content(star_1.age)
       expect(page).to have_content(star_1.size)
       expect(page).to have_content(star_1.can_nova)
+
+      visit "/stars/#{star_2.id}"
+
+      expect(page).to have_content(star_2.name)
+      expect(page).to have_content(star_2.star_type)
+      expect(page).to have_content(star_2.age)
+      expect(page).to have_content(star_2.size)
+      expect(page).to have_content(star_2.can_nova)
+
+      visit "/stars/#{star_3.id}"
+
+      expect(page).to have_content(star_3.name)
+      expect(page).to have_content(star_3.star_type)
+      expect(page).to have_content(star_3.age)
+      expect(page).to have_content(star_3.size)
+      expect(page).to have_content(star_3.can_nova)
     end
   end
 end
