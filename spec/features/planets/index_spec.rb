642614b9865e17beb84b9c45cb13328168578df4
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-22 18:34:38 -0600

    fix: correct planet index test

--- spec/features/planets/index_spec.rb
+++ spec/features/planets/index_spec.rb
@@ -8,7 +8,8 @@ require 'rails_helper'
 
 RSpec.describe '/planets' do
   describe 'as a visitor when vist "/planets" I see the index page' do
-    let!(:planet_1) {Planet.create! name:"Earth", planet_type:"rocky", diameter:7917, num_moons:1, has_life:true}
+    let!(:star_1) {Star.create! name: "Sol", star_type: "yellow dwarf star", age: 4.6, size: "1.9885 to 10^30 kg", can_nova: false}
+    let!(:planet_1) {Planet.create! name:"Earth", planet_type:"rocky", diameter:7917, num_moons:1, has_life:true, star: star_1}
 
     it 'can visit the planets index page' do
       visit "/planets"
@@ -19,5 +20,5 @@ RSpec.describe '/planets' do
       expect(page).to have_content(planet_1.num_moons)
       expect(page).to have_content(planet_1.has_life)
     end
-    
   end
+end
