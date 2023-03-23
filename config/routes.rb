a9e772a725236b606387d571163a10cab4a468ec
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-22 18:46:35 -0600

    feat: create child index, user story 3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/stars', to: 'stars#index'
   get '/stars/:id', to: 'stars#show'
-  get 
+  get '/planets', to: 'planets#index'
 end
--- spec/features/planets/index_spec.rb
+++ spec/features/planets/index_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-# User Story 3, Child Index [ ] done
+# User Story 3, Child Index [x] done
 
 # As a visitor
 # When I visit '/child_table_name'
@@ -10,6 +10,7 @@ RSpec.describe '/planets' do
   describe 'as a visitor when vist "/planets" I see the index page' do
     let!(:star_1) {Star.create! name: "Sol", star_type: "yellow dwarf star", age: 4.6, size: "1.9885 to 10^30 kg", can_nova: false}
     let!(:planet_1) {Planet.create! name:"Earth", planet_type:"rocky", diameter:7917, num_moons:1, has_life:true, star: star_1}
+    let!(:planet_2) {Planet.create! name:"Mars", planet_type:"rocky", diameter:4212, num_moons:2, has_life:false, star: star_1}
 
     it 'can visit the planets index page' do
       visit "/planets"
@@ -19,6 +20,12 @@ RSpec.describe '/planets' do
       expect(page).to have_content(planet_1.diameter)
       expect(page).to have_content(planet_1.num_moons)
       expect(page).to have_content(planet_1.has_life)
+
+      expect(page).to have_content(planet_2.name)
+      expect(page).to have_content(planet_2.planet_type)
+      expect(page).to have_content(planet_2.diameter)
+      expect(page).to have_content(planet_2.num_moons)
+      expect(page).to have_content(planet_2.has_life)
     end
   end
 end
