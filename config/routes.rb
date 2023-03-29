e35c8503b18c2b85b7f0be15269f29709d8d2b6d
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-29 16:03:44 -0600

    fix: add a route and correct some formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'stars#index'
   get '/stars', to: 'stars#index'
   get '/stars/:star_id/planets', to: 'star_planets#index'
   get '/stars/:star_id/planets/new', to: 'star_planets#new'
